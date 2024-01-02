dd9612e44e2b6a810d51021e189025f49e9578ab
Prosper Atu <atu.prosper@gmail.com>
2024-01-02 12:27:37 -0800

    task 2 - print lowercase ascii alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Program to print the ASCII alphabet in lowercase"""
 
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+for alphabet in range(97, 123):
+    print("{}".format(chr(alphabet)), end="")
