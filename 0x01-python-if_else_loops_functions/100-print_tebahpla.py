64b53c04439403830eeb898043b8e67ff3efd882
Prosper Atu <atu.prosper@gmail.com>
2024-01-02 14:04:37 -0800

    task 14 - print reverse ASCII

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Program that prints  the ASCII alphabet, in reverse order,
+alternating lowercase and uppercase (z in lowercase and Y in uppercase)
+"""
+
+for i in 'zYxWvUtSrQpOnMlKjIhGfEdCbA':
+    print("{:s}".format(i), end="")
