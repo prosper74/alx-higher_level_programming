20c3fe87edd6def9d194c0f71b377ee72e67fb7f
Prosper Atu <atu.prosper@gmail.com>
2024-01-03 02:49:44 -0800

    task 8 - fixing print error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-function that prints a string in uppercase
+function that converts a string to uppercase
 """
 
 
-def uppercase(str):
-    """Print a string in uppercase."""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print("")
+def uppercase(s):
+    result = ""
+    for char in s:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            uppercase_ascii = ascii_value - 32
+            result += "{}".format(chr(uppercase_ascii))
+        else:
+            result += "{}".format(char)
+    print(result)
