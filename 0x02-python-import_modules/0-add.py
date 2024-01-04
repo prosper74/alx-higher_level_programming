40dedc2483be2ed10d16df7d4ea515252c0126ab
Prosper Atu <atu.prosper@gmail.com>
2024-01-04 03:29:10 -0800

    task 0 - fix checker error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,14 +3,8 @@
 Program that imports the function def add(a, b):
 from the file add_0.py and prints the result of the addition 1 + 2 = 3
 """
-from add_0 import add
-
-
-def main():
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
-
-if __name__ == "__main__":
-    main()
