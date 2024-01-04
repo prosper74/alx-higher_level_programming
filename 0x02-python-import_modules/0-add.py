0848be7e6f184e9b100e1ec94d0bbd69e764ee31
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-04 15:03:30 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-Program that imports the function def add(a, b):
-from the file add_0.py and prints the result of the addition 1 + 2 = 3
-"""
 if __name__ == "__main__":
     from add_0 import add
     a = 1
