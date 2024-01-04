b7b26907723b89b2927798b2b25fbb3d51605949
Prosper Atu <atu.prosper@gmail.com>
2024-01-04 05:57:21 -0800

    load variable

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Program that imports the variable 'a' from the file
+variable_load_5.py and prints its value.
+"""
+from variable_load_5 import a
+
+
+def main():
+    print("{}".format(a))
+
+
+if __name__ == "__main__":
+    main()
