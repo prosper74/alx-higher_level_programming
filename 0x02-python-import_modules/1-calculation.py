8cb45baf9148cc92fb29c4830f0eb0120b93924e
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-04 15:06:47 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""
-Program that that imports functions from the file calculator_1.py,
-does some Maths, and prints the result.
-"""
+
 from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
+
+
+def main():
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
+
