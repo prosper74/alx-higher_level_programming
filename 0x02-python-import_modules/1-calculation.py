7744632635d84a6033a9360abc40cfcf99913756
Prosper Atu <atu.prosper@gmail.com>
2024-01-04 03:33:59 -0800

    task 2 - import and perform maths operations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,13 @@
-fsdf
+#!/usr/bin/python3
+"""
+Program that that imports functions from the file calculator_1.py,
+does some Maths, and prints the result.
+"""
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
