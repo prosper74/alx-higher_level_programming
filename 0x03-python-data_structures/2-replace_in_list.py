6b8b6c125a12e3361f26f0979b6afca746a97a6c
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-06 07:51:37 +0100

    task 2 - fixing output error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in range(len(dir(my_list))):
-        if i == idx:
-            my_list[i] = element
+    count = 0
+    for ele in my_list:
+        if count == idx:
+            my_list[count] = element
             return (my_list)
+        count += 1
     return (my_list)
