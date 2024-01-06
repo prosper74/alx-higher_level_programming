f0190aa5d616801766b5149f97360f33948b62e2
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-06 08:02:49 +0100

    task 5 - no print

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
-t save
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char not in ('c', 'C'):
+            new_string += char
+    return (new_string)
