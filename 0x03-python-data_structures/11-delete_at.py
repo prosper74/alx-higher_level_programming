e1faa7533e15e54f688fffd9babfd2a8a37496cc
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-08 22:21:00 +0100

    task 11 - fixing output error

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or not my_list:
         return my_list
 
-    new_list = my_list[:idx] + my_list[idx + 1:]
+    new_list = del my_list[idx]
 
     return new_list
