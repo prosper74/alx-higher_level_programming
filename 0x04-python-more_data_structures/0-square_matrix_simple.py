7398c9a6317b984d74c6e28190255abf375abba9
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-01-10 17:10:04 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,7 +5,7 @@ def square_matrix_simple(matrix=[]):
     result_matrix = []
 
     for row in matrix:
-        squared_row = [lambda num: num ** 2, row]
+        squared_row = [num ** 2 for num in row]
         result_matrix.append(squared_row)
 
     return result_matrix
