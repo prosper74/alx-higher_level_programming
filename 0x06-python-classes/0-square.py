194b3362e47392f9a0e76fa23b7613d840b22110
Prosper Atu <atu.prosper@gmail.com>
2024-01-28 03:37:24 -0800

    task 0 - sqaure

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+The module is part of the Python test driven development
+This module contains one Class: Square.
+Define a class Square.
+"""
+
+
+class Square:
+    """
+    An empty class Square that defines a square
+    """
+    pass
