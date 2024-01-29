a213abab653cc580b053310ed0c5c4c80df0d268
Prosper Atu <atu.prosper@gmail.com>
2024-01-29 02:05:35 -0800

    task 0 - rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+The module is part of the Python Classes project
+This module contains one Class: Rectangle.
+Define a class Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty class Rectangle that defines a Rectangle
+    """
+    pass
