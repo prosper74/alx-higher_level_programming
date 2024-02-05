c908ca7f09a18e10d69c1cff55bc6283e28b322d
Prosper Atu <atu.prosper@gmail.com>
2024-02-05 12:19:34 -0800

    task 5 - base geometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module is part of the Python Inheritance project
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Represent base geometry.
+    An empty class BaseGeometry
+    """
+    pass
