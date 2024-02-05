4307e5ef3e100df9f09d72a5b57bdb0279d6ee01
Prosper Atu <atu.prosper@gmail.com>
2024-02-05 12:48:43 -0800

    task 7 - fix requirement error

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,14 +9,14 @@ class BaseGeometry:
     """
     Represent base geometry class.
     """
+
     def area(self):
         """
         Not implemented.
         Raises an exception
         """
         raise Exception("area() is not implemented")
-    
-    
+
     def integer_validator(self, name, value):
         """
         Validate a parameter as an integer.
@@ -27,7 +27,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
