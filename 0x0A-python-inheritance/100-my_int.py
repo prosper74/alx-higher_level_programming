f515ab99784a198be4bdd70a0df51e082e7a8306
Prosper Atu <atu.prosper@gmail.com>
2024-02-05 13:23:23 -0800

    task 12 - my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module is part of the Python Inheritance project
+Defines a class MyInt that inherits from int.
+"""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
