6b4d8971802ba943da903f4c34917ee3f23e63ba
Prosper Atu <atu.prosper@gmail.com>
2024-02-05 12:09:10 -0800

    task 2 - fixed error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
 This module is part of the Python Inheritance project
-Defines a class and inherited class-checking function.
+Defines a class-checking function.
 """
 
 
-def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance or inherited instance of a class.
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
 
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
     Returns:
-        If obj is an instance or inherited instance of a_class - True.
+        If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
