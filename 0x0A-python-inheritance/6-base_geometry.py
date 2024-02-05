642d41d7fa0fef5de007bd0af18e188def5bb894
Prosper Atu <atu.prosper@gmail.com>
2024-02-05 12:28:34 -0800

    task 6 - base geometry exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module is part of the Python Inheritance project
+Defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Represent base geometry class.
+    """
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
