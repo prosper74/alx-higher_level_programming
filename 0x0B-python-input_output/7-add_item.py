c17327c1d5fc2e8a668372313fa9d8a728731d26
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-02-06 16:06:04 +0100

    Update 7-add_item.py...

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ and then save them to a file
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
 try:
