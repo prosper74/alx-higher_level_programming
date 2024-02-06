799531f2ab90f96a9eea031949554f2a32a3135c
Prosper Atu <atu.prosper@gmail.com>
2024-02-06 11:58:20 -0800

    task 9 - student json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module is part of the Python Input Output project.
+A class Student that defines a student by
+public instance attributes.
+"""
+
+
+class Student:
+    """
+    class Student that defines a student by public
+    instance attributes - first_name, last_name, age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initialize the Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+        return self.__dict__
