440175e0b02c77d62476ffd5ecc910969649bf1a
Prosper Atu <atu.prosper@gmail.com>
2024-02-06 07:18:44 -0800

    task 8 - class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module is part of the Python Input Output project.
+Defines a function that returns the dictionary description.
+"""
+
+
+def class_to_json(obj):
+    """
+    A function that returns the dictionary description with
+    simple data structure (list, dictionary, string, integer
+    and boolean) for JSON serialization of an object
+    :param filename: The JSON file
+    """
+    return obj.__dict__
+    
