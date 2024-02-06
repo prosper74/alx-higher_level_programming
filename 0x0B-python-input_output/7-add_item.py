9ef2658af1c64a11a99cf5056cbacf850a52fc0e
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-02-06 16:10:21 +0100

    Update 7-add_item.py........

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-"""
-A script that adds all arguments to a Python list,
-and then save them to a file
-"""
-
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-args = sys.argv[1:]
-try:
-    data = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    data = []
-
-data.extend(args)
-save_to_json_file(data, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    import json
+    with open(filename, 'w', encoding='utf-8') as my_file:
+        json.dump(my_obj, my_file)
