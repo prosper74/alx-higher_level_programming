0f66165c948d08ef7cbf73cbf0e3f5a09f0063e6
Prosper Atu <atu.prosper@gmail.com>
2024-02-06 06:10:10 -0800

    task 3 - JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module is part of the Python Input Output project.
+Defines a function that returns JSON.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    A function that returns the JSON representation
+    of an object (string)
+    :param filename: The filename of the file
+    :param text: The JSON representation
+    Returns the JSON representation
+    """
+    return json.dumps(my_obj)
