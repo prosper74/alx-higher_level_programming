742ff428faab9c01daa115b7093621b0143780b4
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-02-06 15:15:37 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -11,8 +11,7 @@ def to_json_string(my_obj):
     """
     A function that returns the JSON representation
     of an object (string)
-    :param filename: The filename of the file
-    :param text: The JSON representation
+    :param my_obj: the JSON object
     Returns the JSON representation
     """
     return json.dumps(my_obj)
