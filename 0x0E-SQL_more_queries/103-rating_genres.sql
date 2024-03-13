3ac03de234dfca5d34d5a51d496626aaab3c0e57
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-03-13 21:21:46 +0100

    Update 103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- SQL query that retrieves the names of TV genres along with
+-- the sum of ratings associated with TV shows belonging to each genre
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
