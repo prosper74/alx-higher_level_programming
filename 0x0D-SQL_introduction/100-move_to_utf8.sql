88afcece6207053fa0f9fcd9b3c23a21274d4456
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-03-13 12:10:06 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,9 @@
 -- SQL statement performs an alteration on the 'first_table'
 -- in the 'hbtn_0c_0' database, converting its character set
 -- to 'utf8mb4' and its collation to 'utf8mb4_unicode_ci'
+-- Reason: This alteration allows the table to store and
+-- handle a wider range of characters and ensures proper
+-- sorting and comparison of Unicode data.
 USE `hbtn_0c_0`
 ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
