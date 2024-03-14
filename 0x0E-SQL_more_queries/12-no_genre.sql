65df04adc4ccde0fac11be3e10e12e9ffa7a1e7e
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-03-14 12:22:01 +0100

    Update 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- Lists all rows of a database that don't have one column
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
