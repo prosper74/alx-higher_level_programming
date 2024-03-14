a666420b9a2374e3dc108eb9c6010a98a7db7483
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-03-14 12:26:10 +0100

    Update 13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all genres from the database.
+-- Displays the number of shows linked to each.
+-- Lists all rows of a database meeting a condition.
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
