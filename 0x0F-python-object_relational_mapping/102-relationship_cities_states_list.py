3acbb7000d8e9b680150f6f9aecebbcaeeb89b66
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-04-13 17:42:33 +0100

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -3,9 +3,10 @@
 Script that lists all City objects from the database hbtn_0e_101_usa
 """
 from sys import argv
-from relationship_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
 
 if __name__ == "__main__":
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in session.query(State).order_by(State.id):
-        for city_instance in instance.cities:
-            print(city_instance.id, city_instance.name, sep=": ", end="")
-            print(" -> " + instance.name)
+    cities = session.query(City).order_by(City.id).all()
+
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
