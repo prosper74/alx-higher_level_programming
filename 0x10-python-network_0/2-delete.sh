30eb64f5090cc593b0d6f66b2bcc3066d53db05a
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-04-25 14:50:33 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# Send a DELETE request to a given URL and display the response body.
 curl -sX DELETE "$1"
