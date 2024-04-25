962c2d1f1ca1b08f8da7e157910e35dd3e316766
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-04-25 14:49:59 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Get the response body for a given URL for 200 status code responses.
 curl -sL "$1"
