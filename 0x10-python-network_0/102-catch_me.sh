82129fd31baab4c34b7a117b4e9eb88fe44e65ba
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-04-25 15:28:05 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -X PUT -s 0.0.0.0:5000/catch_me --output /dev/null --header "Content-Type: text/plain" --data "You got me!"
