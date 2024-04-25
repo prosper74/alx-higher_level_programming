ef82b3c797bd8f18f4e9d4097019723ce109f4cc
Prosper Atu <67523206+prosper74@users.noreply.github.com>
2024-04-25 14:50:59 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL and displays all HTTP methods the server will accept.
+# Display all HTTP methods the server of a given URL will accept.
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
