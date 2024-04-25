8012f4c3b71d04f30144204e90377ecfd688d76f
Prosper Atu <atu.prosper@gmail.com>
2024-04-25 16:08:13 +0100

    TAsk 9 isnt working

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!"
-curl -s 0.0.0.0:5000/catch_me | sed -n '/You got me!/p'
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -X PUT -s 0.0.0.0:5000/catch_me --output /dev/null --header "Content-Type: text/plain" --data "You got me!"
