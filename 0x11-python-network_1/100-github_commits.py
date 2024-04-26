ba83f291f2ef2043aa51e269a829bc25eda2b5f3
Prosper Atu <atu.prosper@gmail.com>
2024-04-26 16:19:32 +0100

    updated task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,20 +9,20 @@ Requirements:
 - You must use the packages `requests` and `sys`
 """
 
-import sys
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    URI = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[2], argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
+    r = requests.get(URI)
+    commits_data = r.json()
     try:
         for i in range(10):
             print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                commits_data[i].get("sha"),
+                commits_data[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
